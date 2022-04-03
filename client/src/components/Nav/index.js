eb90e8c784e500962bc4c0579f73ee28cd31e4e1
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-03 17:22:07 -0400

    changing website name

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="shopping bag">🌼</span>
+          -Flowers-To-Go-🌺
         </Link>
       </h1>
 
