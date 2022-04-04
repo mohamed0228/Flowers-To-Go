be07b13a5af51adae77ce1542a1b6921c679f71d
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-04 10:26:15 -0400

    deleting commented out code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,38 +16,6 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-// export const SORT_PRODUCTS_DESC = gql`
-//   query sortProducts {
-//     products(order_by: {price: desc}) {
-//       _id
-//       name
-//       description
-//       price
-//       quantity
-//       image
-//       category {
-//         _id
-//       }
-//     }
-//   }
-// `
-
-// export const SORT_PRODUCTS_ASC = gql`
-//   query sortProducts {
-//     products(order_by: {price: asc}) {
-//       _id
-//       name
-//       description
-//       price
-//       quantity
-//       image
-//       category {
-//         _id
-//       }
-//     }
-//   }
-// `
-
 export const QUERY_CHECKOUT = gql`
   query getCheckout($products: [ID]!) {
     checkout(products: $products) {
