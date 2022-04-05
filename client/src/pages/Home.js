1a0752b44c8364e1a7169b700e5dde32efd9acd9
mohamed0228 <mohamed0228@outlook.fr>
2022-04-05 18:47:55 -0400

    commiting changes to merge carousel branch

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="container">
      
-      <ProductList />
+      {/* <ProductList /> */}
       <CategoryMenu />
       <ProductList />
       <Cart />
