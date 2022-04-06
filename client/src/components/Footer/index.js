d310c582b46745c151ea835ce65aa9aab9340bbd
mohamed0228 <mohamed0228@outlook.fr>
2022-04-06 11:19:03 -0400

    addimg instagram icon to the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,8 @@ function Footer(){
         <a href="/"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>   
         <a href="/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a> 
         <a href="mailto:mohamed0228@outlook.fr?"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-email-notifications-justicon-flat-justicon.png"  alt="email" className="icon"/></a>
-        <a href="tel:2013440925"><img src="https://img.icons8.com/external-flat-juicy-fish/60/000000/external-phone-contact-us-flat-flat-juicy-fish-2.png" alt="Github" className="icon"/></a> 
+        <a href="tel:0000000000"><img src="https://img.icons8.com/external-flat-juicy-fish/60/000000/external-phone-contact-us-flat-flat-juicy-fish-2.png" alt="Github" className="icon"/></a> 
+        <a href="/"><img src="https://img.icons8.com/bubbles/50/000000/instagram-new--v2.png"alt="instagram" className="icon"/></a>
         </footer>
     )
 }
